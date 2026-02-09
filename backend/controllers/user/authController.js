9efc5ee866f632de4fc7c972d4ddd7243f7a440f
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2026-02-09 16:21:04 +0530

    make the login for user

--- backend/controllers/user/authController.js
+++ backend/controllers/user/authController.js
@@ -1,5 +1,6 @@
 const User=require('../../models/User');
 const bcrypt=require('bcrypt');
+const jwt=require('jsonwebtoken');
 
 exports.registerUser=async(req,res)=>{
     try{
@@ -24,5 +25,50 @@ exports.registerUser=async(req,res)=>{
 
     }catch(err){
         res.status(500).json({message:err.message});
+    }
+}
+
+exports.loginUser=async(req,res)=>{
+    try{
+        const{email,password}=req.body;
+
+        //check if all fields are filled
+        if(!email || !password){
+            return res.status(400).json({
+                message:"All fields are required"
+            });
+        }
+
+        //check user exist or not
+        const user=await User.findOne({email});
+
+        if(!user) return res.status(500).json({message:"user not found"});
+
+        //check the password
+        const isMatch=await bcrypt.compare(password,user.password);
+
+        if(!isMatch) return res.status(401).json({message:"password dose not matched"});
+
+        //ganrate JWT token
+        const token = jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:"1d"}
+        );
+
+        //seccess message
+        res.status(201).json({
+            message:"login succesful",
+            token,
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email
+            }
+        });
+    }catch(err){
+        res.status(500).json({
+            message:err.message
+        });
     }
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,12 @@
 const express=require('express');
 const{
-    registerUser
+    registerUser,
+    loginUser
     }=require('../controllers/user/authController');
 const router=express.Router();
 
 router.post('/register',registerUser);
+router.post('/login',loginUser);
 
 
 module.exports=router;
