cb08a7a79106b6710deb5c36d416b7c5a4a5a767
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2026-02-10 16:47:08 +0530

    make the frontend side

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+const App=()=>{
+  return(
+    <>
+    <h1>This is home page of the website</h1>
+    </>
+  )
+}
+
+export default App;
---
+++ frontend/src/components/layout/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar=()=>{
+    return(
+        <>
+        <h1>this is the navbar section</h1>
+        </>
+    )
+}
+
+export default Navbar;
