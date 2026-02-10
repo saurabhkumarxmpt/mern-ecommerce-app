b2df4060caf3bbc272292c467de8633da5229b81
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2026-02-10 15:35:18 +0530

    work on the product side

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product=require('../models/Product');
 
 
+//create a new product
 exports.createProduct=async(req,res)=>{
     try{
         const{
@@ -34,6 +35,66 @@ exports.createProduct=async(req,res)=>{
             product
         });
 
+    }catch(err){
+        res.status(500).json({message:err.message});
+    }
+}
+
+
+//find products by category
+exports.getProductsByCategory=async(req,res)=>{
+    try{
+        const {category}=req.params;
+        
+        const products=await Product.findOne({category})
+        .limit(20)
+        .select("name price images");
+
+        res.status(200).json(products);
+
+    }catch(err){
+        res.status(500).json({message:err.message});
+    }
+}
+
+
+//get products for homepage
+exports.getHomePageProducts=async(req,res)=>{
+    try{
+        const featured =await Product.find({isFeatured: true})
+        .limit(6)
+        .select("name price images");
+        
+        const latest=await Product.find()
+        .sort({createdAt: -1 })
+        .limit(6)
+        .select("name price images");
+
+        res.status(200).json({
+            featured,
+            latest
+        });
+     }catch(err){
+        res.status(500).json({message:err.message});
+     }
+
+}
+
+
+exports.getSingleProduct=async(req,res)=>{
+    try{
+
+        const {id}=req.params;
+
+        const product=await Product.findById(id);
+
+        if(!product){
+            return res.status(404).json({
+                message:"product not found"
+            })
+        }
+
+        res.status(200).json(product);
     }catch(err){
         res.status(500).json({message:err.message});
     }
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,11 +1,17 @@
 const express=require('express');
 const{
-    createProduct
+    createProduct,
+    getProductsByCategory,
+    getSingleProduct,
+    getHomePageProducts
 }=require('../controllers/productController');
 
 const router=express.Router();
 
 
 router.post('/create',createProduct)
+router.get('/homepageproducts',getHomePageProducts)
+router.get('/category/:category',getProductsByCategory);
+router.get('/:id',getSingleProduct);
 
 module.exports=router;
