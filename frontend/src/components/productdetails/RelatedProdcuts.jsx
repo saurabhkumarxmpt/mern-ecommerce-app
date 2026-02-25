8cf929a8bde9324f58125c40aaa90c53de515f22
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2026-02-25 23:04:42 +0530

    complete the product detail page

--- frontend/src/components/productdetails/RelatedProdcuts.jsx
+++ frontend/src/components/productdetails/RelatedProdcuts.jsx
@@ -6,14 +6,14 @@ const RelatedProducts = ({ products }) => {
   if (!products || products.length === 0) return null;
 
   return (
-    <div className="max-w-6xl mx-auto px-4 py-16">
+    <div className="max-w-6xl mx-auto px-3 py-16">
 
       {/* Section Heading */}
-      <div className="mb-10">
-        <h2 className="text-3xl font-bold text-gray-800">
+      <div className="mb-10 text-center">
+        <h2 className="text-2xl font-semibold text-green-600">
           Related Products
         </h2>
-        <p className="text-gray-500 mt-2 text-base">
+        <p className="text-gray-500 mt-2 text-sm">
           You may also like these products
         </p>
       </div>
@@ -24,10 +24,10 @@ const RelatedProducts = ({ products }) => {
           <div
             key={item._id}
             onClick={() => navigate(`/products/${item._id}`)}
-            className="cursor-pointer bg-white border rounded-2xl p-4 hover:shadow-xl transition duration-300 group"
+            className="cursor-pointer bg-white border border-gray-200 rounded-sm w-full h-[300px] p-4 hover:shadow-sm transition duration-300 group"
           >
             {/* Product Image */}
-            <div className="h-40 flex items-center justify-center mb-4 overflow-hidden">
+            <div className="h-40 flex items-center justify-center mb-4 overflow-hidden ">
               <img
                 src={item.images?.[0]}
                 alt={item.name}
@@ -36,22 +36,22 @@ const RelatedProducts = ({ products }) => {
             </div>
 
             {/* Product Name */}
-            <h3 className="text-sm font-medium text-gray-700 line-clamp-2">
+            <h3 className="text-md font-medium text-gray-700 line-clamp-3 mb-4">
               {item.name}
             </h3>
 
             {/* Price */}
-            <div className="mt-3 flex items-center gap-2">
-              <span className="text-green-600 font-semibold text-base">
+            <div className="flex items-center gap-2">
+                <span className="text-xl font-bold text-green-600">
                 ₹{item.price}
-              </span>
-
-              {item.oldPrice && (
-                <span className="text-gray-400 text-sm line-through">
-                  ₹{item.oldPrice}
                 </span>
-              )}
-            </div>
+                <span className="text-sm text-gray-400 line-through">
+                ₹{item.price + 500}
+                </span>
+                <span className="text-xs text-red-500 font-semibold">
+                20% OFF
+                </span>
+      </div>
 
           </div>
         ))}
