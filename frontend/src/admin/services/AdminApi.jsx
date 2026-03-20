e74978eaeacb56f68c4a81756460a3b43c669638
Saurabh Kumar <saurabhkumarxmpt@gmail.com>
2026-03-20 15:37:56 +0530

    g

--- frontend/src/admin/services/AdminApi.jsx
+++ frontend/src/admin/services/AdminApi.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API=axios.create({
-    baseURL: "http://localhost:3000/api/admin"
+    baseURL: "https://mern-ecommerce-app-loau.onrender.com/api/admin"
 });
 
 API.interceptors.request.use((req) => {
